d670beb287d521c2a9216866967fe919b0edeba8
Garrett Broadhead <gwbroadhead@gmail.com>
2021-11-16 22:09:12 -0800

    Restarted from scratch. Got most of the functionality implemeneted. Need to validate answer and automatically move on to next question. Timer and High score still needs to be implemented as well as negative time conditions.

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,120 @@
-// Establishing QnA Array
-const qnaArr = [
-  {
-    question: "What does HTML stand for?",
-    answer1: "Correct",
-    answer2: "Wrong",
-    answer3: "Wrong",
-    answer4: "Wrong"
-  },
-  {
-    question: "What does HTML stand for? For reals",
-    answer1: "Correct2",
-    answer2: "Wrong2",
-    answer3: "Wrong2",
-    answer4: "Wrong2"
-  },
-]
+// Variables called by ID
+const start = document.getElementById('startBtn')
+const reset = document.getElementById('resetBtn')
+const questionsContainElem = document.getElementById('questionsContainer')
+const questionElem = document.getElementById('question')
+const answerBtnElem = document.getElementById('answerBtns')
 
-let nextQuestion = 0;
+// Variables for randomized questions and index for it
+let quizRandomized, currentQuestionIndex
 
-
-
-
-
-
-// New Quiz functions
-const newQuiz =() => {
-  document.getElementById('startQuiz').style.display = 'none';
-  let quizQuestions = document.createElement('div')
-  quizQuestions.innerHTML =`
-      <ul class="list-group">
-      <li class="list-group-item">${qnaArr[nextQuestion].question}</li>
-      <li class="list-group-item">${qnaArr[nextQuestion].answer1}</li>
-      <li class="list-group-item">${qnaArr[nextQuestion].answer2}</li>
-      <li class="list-group-item">${qnaArr[nextQuestion].answer3}</li>
-      <li class="list-group-item">${qnaArr[nextQuestion].answer4}</li>
-      </ul> 
-      `
-  document.getElementById('questions').append(quizQuestions)
+// Starts the Quiz and cycles through the Q and A
+start.addEventListener('click', startQuiz)
+function startQuiz() {
+  start.classList.add('hide')
+  quizRandomized = quizQuestions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  reset.classList.remove('hide')
+  questionsContainElem.classList.remove('hide')
+  nextQuestion()
 }
 
-// Start Quiz
-document.getElementById('startQuiz').addEventListener('click', newQuiz);
+// Function for showing questions and answers from array
+//Also handles the logic for finding correct answers
+function showQuestion(question) {
+  questionElem.innerHTML = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerBtnElem.appendChild(button)
+  })
+}
 
+// Function for getting the next question
+function nextQuestion() {
+  resetInfo('')
+  showQuestion(quizRandomized[currentQuestionIndex])
 
-// Cycles through qnaArr to next question
-document.addEventListener('click',event =>{
-  if (event.target.classList.contains('list-group-item')){
-    console.log("list-group-item")
-    nextQuestion ++ 
-    questions.innerHTML= ""
-    newQuiz()
+}
+
+// Function for resetting info to default states
+function resetInfo() {
+  while (answerBtnElem.firstChild) {
+    answerBtnElem.removeChild(answerBtnElem.firstChild)
   }
-  
-})
+}
+
+// Function for selecting and verifying Answers
+function selectAnswer(e) {
+  const userChoice = e.target
+  const correct = userChoice.dataset.correct 
+  setAnswerStatus(document.body, correct)
+  Array.from(answerBtnElem.children).forEach(button => {
+    setAnswerStatus(button, button.dataset.correct)
+  })
+  if (quizRandomized.length > currentQuestionIndex + 1) {
+    currentQuestionIndex++
+  }
+}
+
+// Adds class of correct or incorrect to answers
+function setAnswerStatus(element, correct) {
+  clearAnswerStatus(element)
+  if (correct){
+    element.classList.add('correct')
+  } else {
+    element.classList.add('incorrect')
+  }
+}
+
+// Function for clearing answer status
+function clearAnswerStatus(element) {
+element.classList.remove('correct')
+element.classList.remove('incorrect')
+}
 
 
-
-
-
-
-
-
+// Quiz Questions and Answers array
+const quizQuestions = [
+  {
+    question: "What is 2 +2?",
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', incorrect: false },
+      { text: '22', incorrect: false },
+      { text: '22', incorrect: false }
+    ]
+  }, 
+  {
+    question: "What is 5 + 2?",
+    answers: [
+      { text: '7', correct: true },
+      { text: '22', incorrect: false },
+      { text: '22', incorrect: false },
+      { text: '22', incorrect: false }
+    ]
+  },
+  {
+    question: "What is 5 + 22?",
+    answers: [
+      { text: '7', correct: true },
+      { text: '22', incorrect: false },
+      { text: '255', incorrect: false },
+      { text: '22', incorrect: false }
+    ]
+  },
+  {
+    question: "What is 55 + 22?",
+    answers: [
+      { text: '7', correct: true },
+      { text: '252', incorrect: false },
+      { text: '225', incorrect: false },
+      { text: '2552', incorrect: false }
+    ]
+  }
+]

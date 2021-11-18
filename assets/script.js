f8ec3fdc7fbf7a8f0233ea138bce9bb5e787b55c
Garrett Broadhead <gwbroadhead@gmail.com>
2021-11-17 19:21:54 -0800

    timer added with functions for reducing time on wrong answers,quiz ends when counter reaches 0, and timer only starts when quiz starts.

--- assets/script.js
+++ assets/script.js
@@ -7,80 +7,8 @@ const answerBtnElem = document.getElementById('answerBtns')
 
 let currentQuestionIndex = 0
 
-// Starts the Quiz and cycles through the Q and A
-start.addEventListener('click', startQuiz)
-function startQuiz() {
-  start.classList.add('hide')
-  reset.classList.remove('hide')
-  questionsContainElem.classList.remove('hide')
-  nextQuestion()
-}
-
-// Function for showing questions and answers from array
-// Also handles the logic for finding correct answers
-function showQuestion(questionIndex) {
-  questionElem.innerHTML = quizQuestions[questionIndex].question
-  quizQuestions[questionIndex].answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerBtnElem.appendChild(button)
-  })
-}
-
-// Function for getting the next question
-function nextQuestion() {
-  resetInfo('')
-  if (currentQuestionIndex === quizQuestions.length){
-    questionElem.innerHTML=""
-    // Replace consolelog with end function
-    console.log('You dun it')
-  } else {
-    showQuestion(currentQuestionIndex)
-  }
-}
-
-function endQuiz() {
-
-}
-
-// Function for resetting info to default states
-function resetInfo() {
-  while (answerBtnElem.firstChild) {
-    answerBtnElem.removeChild(answerBtnElem.firstChild)
-  }
-}
-
-// Function for selecting and verifying Answers
-function selectAnswer(e) {
-  const userChoice = e.target.innerText
-  console.log(userChoice)
-  if (userChoice === quizQuestions[currentQuestionIndex].correct) {
-    currentQuestionIndex++
-    nextQuestion()
-  } 
-  
-}
-
-// Adds class of correct or incorrect to answers
-function setAnswerStatus(element, correct) {
-  clearAnswerStatus(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('incorrect')
-  }
-}
-
-// Function for clearing answer status
-function clearAnswerStatus(element) {
-  element.classList.remove('correct')
-  element.classList.remove('incorrect')
-}
+let seconds = 21
+let timer = document.getElementById('time')
 
 // Quiz Questions and Answers array
 const quizQuestions = [
@@ -126,6 +54,105 @@ const quizQuestions = [
   },
 ]
 
-let score = 0
 
-let time = 0
+// Starts the Quiz and cycles through the Q and A
+start.addEventListener('click', startQuiz)
+function startQuiz() {
+  start.classList.add('hide')
+  reset.classList.remove('hide')
+  questionsContainElem.classList.remove('hide')
+  startTime = setInterval(incrementSeconds, 1000)
+  nextQuestion()
+}
+
+// Timer count
+function incrementSeconds() {
+  if (seconds > 0) {
+    seconds -= 1
+    timer.innerText = "Time: " + seconds
+  } else {
+    clearInterval(startTime)
+    endQuiz()
+  }
+}
+
+// Function for showing questions and answers from array
+// Also handles the logic for finding correct answers
+function showQuestion(questionIndex) {
+  questionElem.innerHTML = quizQuestions[questionIndex].question
+  quizQuestions[questionIndex].answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerBtnElem.appendChild(button)
+  })
+}
+
+// Function for getting the next question
+function nextQuestion() {
+  resetInfo('')
+  if (currentQuestionIndex === quizQuestions.length){
+    questionElem.innerHTML=""
+    // Replace consolelog with end function
+    endQuiz()
+  } else {
+    showQuestion(currentQuestionIndex)
+  }
+}
+
+function endQuiz() {
+  if (seconds <= 0) {
+    console.log("Game Over")
+  }
+
+}
+
+// Function for resetting info to default states
+function resetInfo() {
+  while (answerBtnElem.firstChild) {
+    answerBtnElem.removeChild(answerBtnElem.firstChild)
+  }
+}
+
+
+// Function for selecting and verifying Answers
+function selectAnswer(e) {
+  const userChoice = e.target.innerText
+  console.log(userChoice)
+  if (userChoice === quizQuestions[currentQuestionIndex].correct) {
+    currentQuestionIndex++
+    nextQuestion()
+  } else if (userChoice != quizQuestions[currentQuestionIndex].correct) {
+      seconds -= 5
+      currentQuestionIndex++
+      nextQuestion()
+  }
+  
+}
+
+// Adds class of correct or incorrect to answers
+function setAnswerStatus(element, correct) {
+  clearAnswerStatus(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('incorrect')
+  }
+}
+
+// Function for clearing answer status
+function clearAnswerStatus(element) {
+  element.classList.remove('correct')
+  element.classList.remove('incorrect')
+}
+
+
+
+
+
+
+
